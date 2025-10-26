efb5494c99fead4e1b4d4c7c84212ca2bb4e5319
SincapMirza <mirzasincap@gmail.com>
2025-10-26 12:32:23 +0300

    Initial Spring project commit

---
+++ src/main/java/com/ims/nslmotors/NslMotorsApplication.java
@@ -0,0 +1,13 @@
+package com.ims.nslmotors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NslMotorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NslMotorsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ims/nslmotors/NslMotorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ims.nslmotors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NslMotorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
