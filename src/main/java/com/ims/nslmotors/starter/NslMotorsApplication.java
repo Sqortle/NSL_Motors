7798d3da9aecfbca307dac1f9a4bb31fe61dd250
SincapMirza <mirzasincap@gmail.com>
2025-10-26 12:52:12 +0300

    packages created

--- src/main/java/com/ims/nslmotors/starter/NslMotorsApplication.java
+++ src/main/java/com/ims/nslmotors/starter/NslMotorsApplication.java
@@ -1,4 +1,4 @@
-package com.ims.nslmotors;
+package com.ims.nslmotors.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
